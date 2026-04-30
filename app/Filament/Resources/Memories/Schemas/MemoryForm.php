67f6c73098c7bc9ec3c072c22e98d57d3d2cd2b5
Dustin Woodard <dustin.woodard@gmail.com>
2026-04-29 21:11:49 -0600

    Add conditional visibility to Image component in MemoryForm

--- app/Filament/Resources/Memories/Schemas/MemoryForm.php
+++ app/Filament/Resources/Memories/Schemas/MemoryForm.php
@@ -8,6 +8,7 @@ use Filament\Schemas\Components\Form;
 use Filament\Schemas\Components\Image;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Utilities\Get;
 
 class MemoryForm
 {
@@ -54,7 +55,8 @@ class MemoryForm
                                     url: fn ($get) => $get('photo_url'),
                                     alt: 'Memory photo',
                                 )
-                                    ->imageHeight('12rem'),
+                                    ->imageHeight('12rem')
+                                    ->hidden(fn (Get $get): bool => ! $get('photo_url')),
                             ]),
                     ]),
             ]);
