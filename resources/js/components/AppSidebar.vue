98cbdbc7a7f3d7cd5ee61b7dc5d9ae38412b7bac
Dustin Woodard <dwoodard@insiderealestate.com>
2026-06-05 16:51:13 -0600

    Remove Dashboard link from AppSidebar and update logo link to home

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,15 +14,11 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+
 import type { NavItem } from '@/types';
 
 const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard(),
-        icon: LayoutGrid,
-    },
+    
 ];
 
 const footerNavItems: NavItem[] = [
@@ -45,7 +41,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="dashboard()">
+                        <Link href="/">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
